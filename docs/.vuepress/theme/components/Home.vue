64db676c1ca2a02fa3773b0b7843655246a1a834
xiaoxuefengnian <1173212332@qq.com>
2020-04-01 10:19:02 +0800

    修改home页时间样式

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -134,199 +134,164 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-.home {
-  padding: $navbarHeight 2rem 0;
-  max-width: 960px;
-  margin: 0px auto;
-  display: block;
+<style lang="stylus" scope>
+.home
+  padding: $navbarHeight 2rem 0
+  max-width: 960px
+  margin: 0px auto
+  display: block
 
-  .main-content {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-start;
-    align-content: stretch;
-    justify-content: space-around;
-  }
+.main-content
+  display: flex
+  flex-wrap: wrap
+  align-items: flex-start
+  align-content: stretch
+  justify-content: space-around
 
-  .hero {
-    text-align: center;
+.hero
+  text-align: center
 
-    img {
-      max-width: 100%;
-      max-height: 280px;
-      display: block;
-      margin: 3rem auto 1.5rem;
-      border-radius: 50%;
-      box-shadow: 0 10px 5px #999;
-    }
+  img
+    max-width: 100%
+    max-height: 280px
+    display: block
+    margin: 3rem auto 1.5rem
+    border-radius: 50%
+    box-shadow: 0 10px 5px #999
 
-    h1 {
-      font-size: 3rem;
-    }
+  h1
+    font-size: 3rem
 
-    h1, .description, .action {
-      margin: 1.8rem auto;
-    }
+  h1, .description, .action
+    margin: 1.8rem auto
 
-    .description {
-      max-width: 35rem;
-      font-size: 1.6rem;
-      line-height: 1.3;
-      color: lighten($textColor, 40%);
-    }
+  .description
+    max-width: 35rem
+    font-size: 1.6rem
+    line-height: 1.3
+    color: lighten($textColor, 40%)
 
-    .action-button {
-      display: inline-block;
-      font-size: 1.2rem;
-      color: #fff;
-      background-color: $accentColor;
-      padding: 0.8rem 1.6rem;
-      border-radius: 4px;
-      transition: background-color 0.1s ease;
-      box-sizing: border-box;
-      border-bottom: 1px solid darken($accentColor, 10%);
+  .action-button
+    display: inline-block
+    font-size: 1.2rem
+    color: #fff
+    background-color: $accentColor
+    padding: 0.8rem 1.6rem
+    border-radius: 4px
+    transition: background-color 0.1s ease
+    box-sizing: border-box
+    border-bottom: 1px solid darken($accentColor, 10%)
 
-      &:hover {
-        background-color: lighten($accentColor, 10%);
-      }
-    }
-  }
+    &:hover
+      background-color: lighten($accentColor, 10%)
 
-  .last-updated-files {
-    margin-top: 0rem;
-    margin-left: 2rem;
+.last-updated-files
+  margin-top: 0rem
+  margin-left: 2rem
 
-    h3 {
-      margin: 2rem 0 0.5rem;
-    }
+  h3
+    margin: 2rem 0 0.5rem
 
-    .links {
-      max-height: 360px;
-      overflow: auto;
+  .links
+    max-height: 360px
+    overflow: auto
 
-      .link {
-        user-select: none;
-        padding: 2px 0;
+    .link
+      user-select: none
+      padding: 2px 0
 
-        > span {
-          font-size: 12px;
-          color: #909399;
-        }
+      > span 
+        display: inline-block
+        width: 100px
+        font-size: 12px
+        color: #909399
 
-        > i {
-          font-size: 12px;
-          color: #909399;
-        }
-      }
-    }
-  }
+      > i
+        font-size: 12px
+        color: #909399
+        margin-right: 5px
 
-  .features {
-    border-top: 1px solid $borderColor;
-    padding: 1.2rem 0;
-    margin-top: 2.5rem;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-start;
-    align-content: stretch;
-    justify-content: space-between;
-  }
+.features
+  border-top: 1px solid $borderColor
+  padding: 1.2rem 0
+  margin-top: 2.5rem
+  display: flex
+  flex-wrap: wrap
+  align-items: flex-start
+  align-content: stretch
+  justify-content: space-between
 
-  .feature {
-    flex-grow: 1;
-    flex-basis: 30%;
-    max-width: 30%;
+.feature
+  flex-grow: 1
+  flex-basis: 30%
+  max-width: 30%
 
-    h2 {
-      font-size: 1.4rem;
-      font-weight: 500;
-      border-bottom: none;
-      padding-bottom: 0;
-      color: lighten($textColor, 10%);
-    }
+  h2
+    font-size: 1.4rem
+    font-weight: 500
+    border-bottom: none
+    padding-bottom: 0
+    color: lighten($textColor, 10%)
 
-    p {
-      color: lighten($textColor, 25%);
+  p
+    color: lighten($textColor, 25%)
 
-      &:last-of-type {
-        padding-right: 1rem;
-        text-align: right;
-      }
-    }
-  }
+    &:last-of-type
+      padding-right: 1rem
+      text-align: right
 
-  .footer {
-    padding: 2.5rem;
-    border-top: 1px solid $borderColor;
-    text-align: center;
-    color: lighten($textColor, 25%);
-  }
-}
+.footer
+  padding: 2.5rem
+  border-top: 1px solid $borderColor
+  text-align: center
+  color: lighten($textColor, 25%)
 
-@media (max-width: $MQMobile) {
-  .home {
-    .hero {
-      width: 100%;
-    }
+@media (max-width: $MQMobile)
+  .home
+    .hero
+      width: 100%
+  
+    .last-updated-files
+      width: 100%
+      margin-top: 0
+  
+    .features
+      flex-direction: column
+  
+    .feature
+      max-width: 100%
+      padding: 0 2.5rem
 
-    .last-updated-files {
-      width: 100%;
-      margin-top: 0;
-    }
+@media (max-width: $MQMobileNarrow)
+  .home
+    padding-left: 1.5rem
+    padding-right: 1.5rem  
 
-    .features {
-      flex-direction: column;
-    }
+    .hero
+      width: 100%  
 
-    .feature {
-      max-width: 100%;
-      padding: 0 2.5rem;
-    }
-  }
-}
+      img
+        max-height: 210px
+        margin: 2rem auto 1.2rem  
 
-@media (max-width: $MQMobileNarrow) {
-  .home {
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
+      h1
+        font-size: 2rem  
 
-    .hero {
-      width: 100%;
+      h1, .description, .action
+        margin: 1.2rem auto  
 
-      img {
-        max-height: 210px;
-        margin: 2rem auto 1.2rem;
-      }
+      .description
+        font-size: 1.2rem  
 
-      h1 {
-        font-size: 2rem;
-      }
+      .action-button
+        font-size: 1rem
+        padding: 0.6rem 1.2rem  
 
-      h1, .description, .action {
-        margin: 1.2rem auto;
-      }
+    .last-updated-files
+      width: 100%
+      margin-top: 0  
 
-      .description {
-        font-size: 1.2rem;
-      }
-
-      .action-button {
-        font-size: 1rem;
-        padding: 0.6rem 1.2rem;
-      }
-    }
-
-    .last-updated-files {
-      width: 100%;
-      margin-top: 0;
-    }
-
-    .feature {
-      h2 {
-        font-size: 1.25rem;
-      }
-    }
-  }
-}
+    .feature
+      h2
+        font-size: 1.25rem
 </style>
